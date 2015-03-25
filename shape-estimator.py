89967f170d9dc4126cbae2382e43a4c85ee4f803
Roman Shkarin <ud9751@anka-visu3.anka.kit.edu>
2015-03-25 18:46:30 +0100

    Cgs3

--- shape-estimator.py
+++ shape-estimator.py
@@ -1,26 +1,26 @@
 from ij import IJ, ImagePlus, ImageStack
 from ij.io import TiffDecoder, FileSaver
 from ij.plugin.frame import RoiManager
-from ij.process import ImageStatistics as IS
-from ij.plugin.filter import ParticleAnalyzer as PA
+from ij.process import ImageStatistics, ByteProcessor
+from ij.plugin.filter import ParticleAnalyzer
 from ij.measure import ResultsTable, Measurements
 from java.lang import Double
 import os
 import sys
 import datetime
 
-#inputPath = "D:\\Roman\\XRegio\\Segmentations_Median"
-#outputPath = "D:\\Roman\\XRegio\\Results"
+inputPath = "D:\\Roman\\XRegio\\Segmentations2"
+outputPath = "D:\\Roman\\XRegio\\Results"
 
-inputPath = "/Users/Roman/Documents/test_segmentations";
-outputPath = "/Users/Roman/Documents/test_results";
+#inputPath = "/Users/Roman/Documents/test_segmentations";
+#outputPath = "/Users/Roman/Documents/test_results";
 
 methodPrefix = "segmented_median"
 fishPrefix = "fish"
 fileExt = ".tif"
 statisticsOutputExt = ".csv"
-fishNumbers = ["200"]
-#fishNumbers = ["3000"];
+#fishNumbers = ["200"]
+fishNumbers = ["3000"];
 #fishNumbers = ["200", "202","204","214","215","221","223","224","226","228","230","231","233","235","236","237","238","239","243","244","245"];
 
 sliceStep = 10 #in percentage
@@ -34,14 +34,15 @@ def printLog(title, message):
 
 def getStackBoundaries(imp):
 	fisrtSlice, lastSlice = 0, 0
+	print list(imp.getImageStack().getProcessor(151).convertToByteProcessor().getPixels())
 	
 	for i in range(imp.getStackSize()):
-		if sum(imp.getStack().getProcessor(i + 1).getPixels()) > 0:
+		if sum(imp.getImageStack().getProcessor(i + 1).getPixels()) > 0:
 			fisrtSlice = i + 1
 			break
 
 	for i in reversed(range(imp.getStackSize())):
-		if sum(imp.getStack().getProcessor(i + 1).getPixels()) > 0:
+		if sum(imp.getImageStack().getProcessor(i + 1).getPixels()) > 0:
 			lastSlice = i + 1
 			break
 			
@@ -59,40 +60,26 @@ def getMaximumAreaStack(imp, voxelSize):
 	totalVolumeArea = 0
 
 	for i in range(imp.getStackSize()):
+		IJ.showProgress(i, imp.getStackSize() + 1)  
+		
 		imp.setSliceWithoutUpdate(i + 1)
 		pa.analyze(imp)
-		print rt.getRowAsString(i + 1)
- 
-
-	#return totalVolumeArea, ImagePlus("max_area_stack_" + imp.getTitle(), maxAreaStack)
-	return totalVolumeArea
-
-"""
-	for i in range(imp.getStackSize()):
-		imp.setSliceWithoutUpdate(i + 1)
-
-		pa.analyze(imp)
-	
 		countMask = pa.getOutputImage()
 
-		print rt.getColumn(ResultsTable.AREA)
+		area = rt.getColumn(ResultsTable.AREA)
+		if area:
+			maxArea = max(area)
+			maxAreaId = area.index(maxArea) + 1
+			filteredPixels = map(lambda x: 0 if x != maxAreaId else x, countMask.getProcessor().getPixels())
+			impFiltered = ByteProcessor(imp.getWidth(), imp.getHeight(), filteredPixels)
+			maxAreaStack.addSlice(impFiltered)
+			totalVolumeArea += maxArea / (voxelSize[0] * voxelSize[1])
 
-		#pa.saveResults(countMask.getStatistics(ME.AREA), countMask.getRoi())
-		#print countMask.getStatistics(ME.AREA)
-		#resultTable = ResultsTable.getResultsTable()
-		#print resultTable
-		#area = resultTable.getColumnAsDoubles(resultTable.getColumnIndex("Area"))
-		#maxArea = max(area)
-		#maxAreaId = area.index(maxArea) + 1
+		rt.reset()
 
-		#filteredPixels = map(lambda x: 0 if x != maxAreaId else x, countMask.getPixels())
-		#ipFiltered = ByteProcessor(imp.getWidth(), imp.getHeight(), filteredPixels)  
+	IJ.showProgress(1)
 
-		#maxAreaStack.addSlice(ipFiltered)
-		#totalVolumeArea += maxArea / (voxelSize[0] * voxelSize[1])
-
-	IJ.showProgress(1) 
-"""
+	return totalVolumeArea, ImagePlus("max_area_stack_" + imp.getTitle(), maxAreaStack)
 
 def estimateShape(inputPath, outputPath, sliceStep, fishPrefix, fileExt, methodPrefix, statisticsOutputExt, fishNumbers, sliceNeighbours):
 	for fishNumber in fishNumbers:
@@ -123,9 +110,10 @@ def estimateShape(inputPath, outputPath, sliceStep, fishPrefix, fileExt, methodP
 		print voxelSize
 
 		firstZBound, lastZBound = getStackBoundaries(imp)
+		print "f = %d, l = %d" % (firstZBound, lastZBound)
 		
-		#totalVolumeArea, impMaxAreaStack = getMaximumAreaStack(imp, voxelSize)
-		totalVolumeArea = getMaximumAreaStack(imp, voxelSize)
+		totalVolumeArea, impMaxAreaStack = getMaximumAreaStack(imp, voxelSize)
+		#totalVolumeArea = getMaximumAreaStack(imp, voxelSize)
 		print "totalVolumeArea = %d" % totalVolumeArea
 
 		#fs = FileSaver(impMaxAreaStack)
